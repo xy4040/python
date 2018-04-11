711911e5fdf861a48a40bf77434f17658db68e2c
xy4040 <xy_404@foxmail.com>
2018-04-12 01:25:44 +0800

    采集网络图片并保存到本地

--- f2.py
+++ f2.py
@@ -4,6 +4,7 @@ url = 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=523935819,4249262043&fm=58&
 r = requests.get(url)
 print(r.url)
 
+# 这里打开二进制文件，选择的方式用'wb+'
 with open('1.jpg','wb+') as f:
 	f.write(r.content)
 
